7dca600538b76d93abfc55bab8497fcb3b6f7d7f
Relsi Maron <relsi@codeup.com.br>
2021-03-09 18:51:10 -0300

    feat (frontend): add a simple layout on index homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,180 @@
-import { Col, Input, InputGroup, InputGroupAddon, Row } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
-function Home() {
+const Home = () => {
   return (
-    <div className="container-fluid">
-      <p>hello</p>
+    <div>
+      <section class="jumbotron text-center">
+        <Row>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>Featured restaurant.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>Featured restaurant.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>Featured restaurant.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>Featured restaurant.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </section>
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>
+                  A little short description about the restaurant.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>
+                  A little short description about the restaurant.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>
+                  A little short description about the restaurant.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Miao Chao</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                  Chinese Food
+                </CardSubtitle>
+                <CardText>
+                  A little short description about the restaurant.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 export default Home;

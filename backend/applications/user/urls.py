9b7b65363db1a682a4d5ea3afd846ba76adef045
Relsi Maron <relsi@codeup.com.br>
2021-03-08 21:14:33 -0300

    feat (backend): add JWT autentication

--- backend/applications/user/urls.py
+++ backend/applications/user/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 user_api_endpoints = [
     url(r'^api/v1/', include('djoser.urls')),
-    url(r'^api/v1/', include('djoser.urls.authtoken')),
+    url(r'^api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api/v1/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
